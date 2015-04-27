4bf5c0543185277c49b79ab651c244c19ecbdffe
Monomachus <monomachus85@yahoo.com>
2015-04-27 10:01:35 +0300

    Cleared template

--- src/org/nbphpcouncil/modules/php/symfony2/filetemplates/Symfony2RepositoryTemplate.php
+++ src/org/nbphpcouncil/modules/php/symfony2/filetemplates/Symfony2RepositoryTemplate.php
@@ -4,14 +4,6 @@ namespace ${namespaceDir};
 
 use Doctrine\ORM\EntityRepository;
 
-/**
- <#if repoDeclared>
- * @ORM\Entity(repositoryClass="${namespaceForRepository}\${name}Repository")
-<#else>
- * @ORM\Entity
-</#if>
- * @ORM\Table(name="${optionalTableName}")
- */
 class ${name}Repository extends EntityRepository
 {
    
