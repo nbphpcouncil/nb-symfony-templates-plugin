0ef3b18f2628d1d5ef31a57ce6118a3b6d31a5ce
junichi11 <junichi0206@gmail.com>
2015-04-27 21:33:24 +0900

    Fix Symfony2RepositoryTemplate.php
    - Prevent an error

--- src/org/nbphpcouncil/modules/php/symfony2/filetemplates/Symfony2RepositoryTemplate.php
+++ src/org/nbphpcouncil/modules/php/symfony2/filetemplates/Symfony2RepositoryTemplate.php
@@ -1,10 +1,12 @@
 <?php
 
+<#if namespaceDir?? && namespaceDir != "">
 namespace ${namespaceDir};
 
+</#if>
 use Doctrine\ORM\EntityRepository;
 
 class ${name}Repository extends EntityRepository
 {
-   
+
 }
